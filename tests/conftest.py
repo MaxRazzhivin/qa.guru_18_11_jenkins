9241fbdba698b0d43500b2ef5b3ef1d3f76a8269
Max_Razzhivin <max.nvo06@gmail.com>
2025-03-17 08:02:16 +0300

    .

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,32 +1,34 @@
 import pytest
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service as ChromeService
 from selene import Browser, Config
 
 from utils import attach
-
 
 @pytest.fixture(scope='function')
 def setup_browser(request):
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "latest",
+        "browserVersion": "100.0",
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
         }
     }
     options.capabilities.update(selenoid_capabilities)
-
     driver = webdriver.Remote(
-        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options
     )
 
     browser = Browser(Config(driver))
     yield browser
 
-    # Закрытие браузера после теста
-    driver.quit()
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
+
+    browser.quit()

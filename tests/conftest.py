6c6ccada1baca84d64ae4045eb5db144f0e2663b
Max_Razzhivin <max.nvo06@gmail.com>
2025-03-17 09:36:38 +0300

    fix conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,8 @@ from utils import attach
 def setup_browser():
     driver_options = webdriver.ChromeOptions()
     driver_options.page_load_strategy = 'eager'
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
 
     options = Options()
     selenoid_capabilities = {

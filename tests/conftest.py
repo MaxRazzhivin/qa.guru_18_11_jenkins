249cb5af68a3812097a79eb19bc30d40e251e458
Max_Razzhivin <max.nvo06@gmail.com>
2025-03-17 09:27:43 +0300

    fix conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,15 @@
 import pytest
-
+from selene import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selene import Browser, Config
-
 from utils import attach
 
 
-@pytest.fixture(scope='function')
-def setup_browser(request):
+@pytest.fixture(scope='function', autouse=True)
+def setup_browser():
+    driver_options = webdriver.ChromeOptions()
+    driver_options.page_load_strategy = 'eager'
+
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
@@ -18,14 +19,15 @@ def setup_browser(request):
             "enableVideo": True
         }
     }
+
     options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        options=options
-    )
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options)
 
-    browser = Browser(Config(driver=driver))
-    yield browser
+    browser.config.driver = driver
+
+    yield
 
     attach.add_screenshot(browser)
     attach.add_logs(browser)
